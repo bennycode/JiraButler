a092d78a80a7d66a88585d416177255289279b87
Benny Neugebauer <bn@bennyn.de>
2011-01-02 01:43:08 +0100

    Some bugfixes.

--- src/de/angelcode/jirabutler/soap/JiraController.java
+++ src/de/angelcode/jirabutler/soap/JiraController.java
@@ -1,12 +1,11 @@
 package de.angelcode.jirabutler.soap;
 
 import com.atlassian.jira.rpc.soap.beans.RemoteVersion;
-import de.angelcode.jirabutler.soap.service.JiraClientImpl;
+import de.angelcode.jirabutler.soap.service.JiraClient;
 import de.angelcode.jirabutler.util.SystemVariables;
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.rmi.RemoteException;
 import java.util.Properties;
 
 /**
@@ -17,18 +16,20 @@ public class JiraController
 {
 
   private String version;
-  private String key;
+  private String issueKey;
   private String username;
   private String message;
   private String connectionUrl;
   private String connectionUsername;
   private String connectionPassword;
+  private String projectKey;
+  private JiraClient client;
 
   public JiraController()
   {
     super();
     this.version = null;
-    this.key = null;
+    this.issueKey = null;
     this.username = null;
     this.message = null;
     this.connectionUrl = null;
@@ -39,11 +40,9 @@ public class JiraController
   public JiraController(String version, String key, String username, String message)
   {
     this.version = version;
-    this.key = key;
+    this.issueKey = key;
     this.username = username;
     this.message = message;
-    // Get JIRA connection details
-
   }
 
   /**
@@ -58,38 +57,44 @@ public class JiraController
     this.connectionUrl = properties.getProperty("url");
     this.connectionUsername = properties.getProperty("username");
     this.connectionPassword = properties.getProperty("password");
+    this.projectKey = properties.getProperty("project-key");
   }
 
   public boolean connect() throws Exception
   {
     boolean isLoggedIn = false;
-    if (this.connectionUsername != null && this.connectionPassword != null && this.connectionUrl != null)
+    if (this.connectionUsername != null
+            && this.connectionPassword != null
+            && this.connectionUrl != null)
     {
-      JiraClientImpl client = new JiraClientImpl();
+      this.client = new JiraClient();
       isLoggedIn = client.login(this.connectionUsername, this.connectionPassword);
-      RemoteVersion newVersion = new RemoteVersion();
-      newVersion.setName(this.version);
-      try
-      {
-        client.addVersion(newVersion);
-      }
-      catch(NoClassDefFoundError ex)
-      {
-        System.out.println("Version successfully set.");
-      }
-      catch(com.atlassian.jira.rpc.exception.RemoteException ex)
-      {
-        System.out.println("Version already set.");
-      }
-      catch(Exception ex)
-      {
-        System.out.println("Version cannot be set.");
-      }
     }
     return isLoggedIn;
   }
 
-  public void addVersion() throws Exception
+  public void addVersion()
   {
+    if (this.projectKey != null && this.version != null)
+    {
+      RemoteVersion newVersion = new RemoteVersion();
+      newVersion.setName(this.version);
+      try
+      {
+        client.addVersion(this.projectKey, newVersion);
+      }
+      catch (NoClassDefFoundError ex)
+      {
+        System.out.println("Version successfully set.");
+      }
+      catch (com.atlassian.jira.rpc.exception.RemoteException ex)
+      {
+        System.out.println("Version already set.");
+      }
+      catch (Exception ex)
+      {
+        System.out.println("Version cannot be set.");
+      }
+    }
   }
 }
--- src/de/angelcode/jirabutler/soap/service/JiraClient.java
+++ src/de/angelcode/jirabutler/soap/service/JiraClient.java
@@ -1,60 +1,132 @@
 package de.angelcode.jirabutler.soap.service;
 
 import com.atlassian.jira.rpc.exception.RemoteAuthenticationException;
+import com.atlassian.jira.rpc.exception.RemotePermissionException;
+import java.rmi.RemoteException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.xml.rpc.ServiceException;
+
+import com.atlassian.jira.rpc.soap.beans.RemoteVersion;
 import de.angelcode.jirabutler.exceptions.JIRAException;
 import de.angelcode.jirabutler.exceptions.VoidParameterException;
+import java.util.ArrayList;
 
 public class JiraClient
 {
 
-  /**
-   * @param args
-   */
-  public static void main(String[] args) throws JIRAException, VoidParameterException, RemoteAuthenticationException, com.atlassian.jira.rpc.exception.RemoteException
+  private JiraSoapServiceService service;
+  private JiraSoapService api;
+  private String token = null;
+
+  public JiraClient() throws ServiceException
   {
-
-
-    try
-    {
-
-      JiraClientImpl jiraClient = new JiraClientImpl();
-
-      System.out.println("Login: " + jiraClient.login("testuser", "5YR1ZL5oyXH2eB6C4sN7"));
-      System.out.println("Logout: " + jiraClient.logout());
-    }
-    catch (Exception ex)
-    {
-      System.out.println(ex.getLocalizedMessage());
-    }
-
-//		JiraSoapServiceService service = new JiraSoapServiceServiceLocator();
-//
-//		try {
-//			JiraSoapService api = service.getJirasoapserviceV2();
-//
-//			String token = api.login("robertb", "test");
-//			System.out.println("Eingeloggt als: " + api.getUser(token, "robertb").getEmail());
-//			RemoteProject project = api.getProjectById(token, 10000);
-//			System.out.println("Alte Versionen:");
-//			RemoteVersion[] versions = api.getVersions(token, project.getKey());
-//			for (RemoteVersion v : versions) {
-//				System.out.println(v.getName());
-//			}
-//			RemoteVersion newVersion = new RemoteVersion();
-//			newVersion.setName(args[0]);
-//			api.addVersion(token, project.getKey(), newVersion);
-//			System.out.println("Neue Versionen:");
-//			versions = api.getVersions(token, project.getKey());
-//			for (RemoteVersion v : versions) {
-//				System.out.println(v.getName());
-//			}
-//			boolean logout = api.logout(token);
-//			System.out.println("Logout erfogreich: " + logout);
-//		} catch (ServiceException e) {
-//			e.printStackTrace();
-//		} catch (RemoteException e) {
-//			e.printStackTrace();
-//		}
-
+    service = (JiraSoapServiceService) new JiraSoapServiceServiceLocator();
+    api = service.getJirasoapserviceV2();
   }
+
+  public void setService(JiraSoapServiceService service)
+  {
+    this.service = service;
+  }
+
+  public JiraSoapServiceService getService()
+  {
+    return service;
+  }
+
+  public JiraSoapService getApi()
+  {
+    return api;
+  }
+
+  public void setApi(JiraSoapService api)
+  {
+    this.api = api;
+  }
+
+  public String getToken()
+  {
+    return token;
+  }
+
+  public void setToken(String token)
+  {
+    this.token = token;
+  }
+
+  public boolean login(String user, String password) throws JIRAException, RemoteException, VoidParameterException, RemoteAuthenticationException, com.atlassian.jira.rpc.exception.RemoteException
+  {
+    if (user == null || user.equals(""))
+    {
+      throw new VoidParameterException("Void value for paramter user");
+    }
+    else
+    {
+      token = api.login(user, password);
+    }
+    return token == null ? false : true;
+  }
+
+  public boolean logout() throws RemoteException
+  {
+    return api.logout(token);
+  }
+
+  public boolean addVersion(String jiraProjectKey, RemoteVersion version) throws RemoteException, com.atlassian.jira.rpc.exception.RemoteException
+  {
+    boolean success = false;
+
+    api.addVersion(token, jiraProjectKey, version);
+    success = true;
+
+    return success;
+  }
+
+  /**
+   * Experimental
+   * @param jiraProjectKey
+   * @return
+   * @throws RemoteException
+   * @throws RemotePermissionException
+   * @throws RemoteAuthenticationException
+   * @throws com.atlassian.jira.rpc.exception.RemoteException
+   */
+  public ArrayList<String> getVersions(String jiraProjectKey) throws RemoteException, RemotePermissionException, RemoteAuthenticationException, com.atlassian.jira.rpc.exception.RemoteException
+  {
+    ArrayList<String> versionList = new ArrayList<String>();
+    RemoteVersion[] versions = api.getVersions(token, jiraProjectKey);
+
+    for (RemoteVersion version:versions)
+    {
+      versionList.add(version.getName());
+    }
+
+    return versionList;
+  }
+//	try {
+//		String token = api.login("robertb", "test");
+//		System.out.println("Eingeloggt als: " + api.getUser(token, "robertb").getEmail());
+//		RemoteProject project = api.getProjectById(token, 10000);
+//		System.out.println("Alte Versionen:");
+//		RemoteVersion[] versions = api.getVersions(token, project.getKey());
+//		for (RemoteVersion v : versions) {
+//			System.out.println(v.getName());
+//		}
+//		RemoteVersion newVersion = new RemoteVersion();
+//		newVersion.setName(args[0]);
+//		api.addVersion(token, project.getKey(), newVersion);
+//		System.out.println("Neue Versionen:");
+//		versions = api.getVersions(token, project.getKey());
+//		for (RemoteVersion v : versions) {
+//			System.out.println(v.getName());
+//		}
+//		boolean logout = api.logout(token);
+//		System.out.println("Logout erfogreich: " + logout);
+//	} catch (ServiceException e) {
+//		e.printStackTrace();
+//	} catch (RemoteException e) {
+//		e.printStackTrace();
+//	}
 }
