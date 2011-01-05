9ec1d3e486c23c839d63773c499ede8b1de7d4cf
Scarfake <robert.benedikt@gmx.de>
2011-01-05 21:19:15 +0100

    some fixes for cobertura

--- test/de/angelcode/jirabutler/util/SystemVariablesTest.java
+++ test/de/angelcode/jirabutler/util/SystemVariablesTest.java
@@ -8,41 +8,47 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- * 
+ *
  * @author BoatoonAdmin
  */
-public class SystemVariablesTest {
+public class SystemVariablesTest
+{
 
-	public SystemVariablesTest() {
-	}
+  public SystemVariablesTest()
+  {
+  }
 
-	@BeforeClass
-	public static void setUpClass() throws Exception {
-	}
+  @BeforeClass
+  public static void setUpClass() throws Exception
+  {
+  }
 
-	@AfterClass
-	public static void tearDownClass() throws Exception {
-	}
+  @AfterClass
+  public static void tearDownClass() throws Exception
+  {
+  }
 
-	@Before
-	public void setUp() {
-	}
+  @Before
+  public void setUp()
+  {
+  }
 
-	@After
-	public void tearDown() {
-	}
+  @After
+  public void tearDown()
+  {
+  }
 
-	/**
-	 * Test of getJarExecutionDirectory method, of class SystemVariables.
-	 */
-	@Test
-	public void testGetJarExecutionDirectory() {
-		System.out.println("getJarExecutionDirectory");
-		String expResult = "";
-		String result = SystemVariables.getJarExecutionDirectory();
-		assertEquals(expResult, result);
-		// TODO review the generated test code and remove the default call to
-		// fail.
-		fail("The test case is a prototype.");
-	}
+  /**
+   * Test of getJarExecutionDirectory method, of class SystemVariables.
+   */
+  @Test
+  public void testGetJarExecutionDirectory()
+  {
+    System.out.println("getJarExecutionDirectory");
+    String expResult = "";
+    String result = SystemVariables.getJarExecutionDirectory();
+    assertEquals(expResult, result);
+    // TODO review the generated test code and remove the default call to fail.
+    fail("The test case is a prototype.");
+  }
 }
