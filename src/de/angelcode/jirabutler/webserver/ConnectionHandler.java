6b44129f2ade310b945e47e1f30f8619d831420d
Benny Neugebauer <bn@bennyn.de>
2011-01-05 03:24:18 +0100

    hook.convertGithubRequestToJson(); macht Probleme :(

--- src/de/angelcode/jirabutler/webserver/ConnectionHandler.java
+++ src/de/angelcode/jirabutler/webserver/ConnectionHandler.java
@@ -1,14 +1,11 @@
 package de.angelcode.jirabutler.webserver;
 
-import de.angelcode.jirabutler.exceptions.JiraButlerException;
 import java.io.BufferedReader;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.Socket;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  * Processes server requests and responses.
